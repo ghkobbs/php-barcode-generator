441eb0ae86053ae53ee5d52ee7a6404c074a4ea1
Maxwell Morrison <maxkobbs@gmail.com>
2019-04-09 15:21:59 +0000

    Personal Addition in index.php

--- index.php
+++ index.php
@@ -6,11 +6,12 @@ include('barcode/BarcodeGeneratorJPG.php');
 include('barcode/BarcodeGeneratorHTML.php');
 
 $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
+$code = "0000000000";
+$qty = 2000;
 
-for ($i = 0; $i < 2000; $i++) {
+for ($i = 1; $i <= $qty; $i++) {
 
-$code = rand(000000000000, 999999999999);
-
-echo $generator->getBarcode($code, $generator::TYPE_EAN_13);
+    $code = sprintf("%'.010d", $i);
+    echo $generator->getBarcode("$code", $generator::TYPE_EAN_13);
 
 }
