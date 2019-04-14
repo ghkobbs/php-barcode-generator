c7bd52e39ba2c0dea105f8abc5a5854d5ca2f95a
Maxwell Morrison <maxkobbs@gmail.com>
2019-04-14 20:32:58 +0000

    Changed the layout to display 3 barcode per row

--- index.php
+++ index.php
@@ -8,12 +8,19 @@ include('barcode/BarcodeGeneratorHTML.php');
 $generator = new Picqer\Barcode\BarcodeGeneratorHTML();
 
 $code = "0000000000";
-$qty = 5;
+$qty = 99;
 $name = "National Banking College";
+
+?>
+
+<div class="wrapper">
+<?php
 
 for ($i = 1; $i <= $qty; $i++) {
 
     $code = sprintf("%'.010d", $i);
-    echo "<p style='width:190px; text-align: center; margin-bottom:0'>".$name."</p>".$generator->getBarcode("$code", $generator::TYPE_EAN_13);
+    echo "<div class='push-left' style='width: calc(100% / 3); float: left'><p style='width:190px; text-align: center; margin:0'>".$name."</p>".$generator->getBarcode("$code", $generator::TYPE_EAN_13)."</div>";
 
-}
+    
+}?>
+</div>
